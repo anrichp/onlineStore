9f4d062825e9fee92ad1b845c39da42133c160c2
Anrich Potgieter <anrichp@gmail.com>
2021-03-30 09:50:44 +0100

    fix typo

--- online store/base.py
+++ online store/base.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.extdeclarative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('sqlite:///:memory:', echo=True)
