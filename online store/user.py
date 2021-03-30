44283d1902eaa3ef0a2c41071c55f7db148ad042
Anrich Potgieter <anrichp@gmail.com>
2021-03-30 09:44:30 +0100

    Create declarative base that can be used throughout all classes

---
+++ online store/user.py
@@ -0,0 +1,39 @@
+from sqlalchemy import Column, String, Integer, Date
+
+from base import Base
+
+
+class User(Base):
+    __tablename__ = 'User'
+    user_id = Column(Integer, primary_key=True)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    email_address = Column(String(120), unique=True, nullable=False)
+    contact_number = Column(String(20), unique=True)
+    type = Column(String(20))
+
+    __mapper_args__ = {
+        'polymorphic_on': type,
+        'polymorphic_identity': 'user'
+    }
+
+
+class Seller(User):
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'seller'
+    }
+
+
+class Customer(User):
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'customer'
+    }
+
+
+class WarehouseStaff(User):
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'warehouseStaff'
+    }
