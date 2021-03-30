9dfdb141db50e6ec4ed6dbada17e088424ef6e39
Anrich Potgieter <anrichp@gmail.com>
2021-03-30 11:50:16 +0100

    more testing

--- online store/app.py
+++ online store/app.py
@@ -1,20 +1,20 @@
-from user import Seller
+from user import User
 from base import Session, engine, Base
 
 Base.metadata.create_all(engine)
 
 session = Session()
 
-Anrich = Seller(first_name='Anrich',
+Anrich = User(first_name='Anrich',
               last_name='Potgieter',
               email_address='anrichp@gmail.com',
-              contact_number='07907451834',)
+              contact_number='07907451834')
 
 session.add(Anrich)
 session.commit()
 session.close()
 
-users = session.query(Seller).all()
+users = session.query(User).all()
 
 for user in users:
     print(user.type)
