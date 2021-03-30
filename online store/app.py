c283fa95e112b369f46c9c9d7ec3f19c9a4c2059
Anrich Potgieter <anrichp@gmail.com>
2021-03-30 21:37:34 +0100

    object formatting

--- online store/app.py
+++ online store/app.py
@@ -6,7 +6,7 @@ Base.metadata.create_all(engine)
 session = Session()
 
 Individual.createUser(first_name='Quintus', last_name='Potgieter', email_address='quintus@gmail.com',
-                        contact_number='0736229189',proof_of_identity='identity', proof_of_banking='banking' )
+                      contact_number='0736229189', proof_of_identity='identity', proof_of_banking='banking')
 
 users = session.query(Individual).all()
 
