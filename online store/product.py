cdc8e650c8153af175e5a4a64e993d08c95ec242
Anrich Potgieter <anrichp@gmail.com>
2021-03-30 22:32:28 +0100

    refactoring the product package

--- online store/product.py
+++ online store/product.py
@@ -5,21 +5,6 @@ from base import Base, engine, Session
 session = Session()
 
 
-class Product(Base):
-    __tablename__ = 'product'
-
-    product_id = Column(Integer, primary_key=True)
-    product_title = Column(String(50), nullable=False)
-    product_description = Column(String(120), nullable=False)
-    product_category = Column(String(50), nullable=False)
-    product_price = Column(Numeric(12, 2), nullable=False)
-    quantity = Column(Numeric(12), nullable=False)
-    product_location = Column(String(50), nullable=False)
-    product_status = Column(String(50), nullable=False)
-    product_catalog_id = Column(Integer, ForeignKey(
-        'productCatalogue.product_catalogue_id'))
-
-
 class ProductCatalogue(Base):
     __tablename__ = 'productCatalogue'
 
