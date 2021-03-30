1e4b1c52b38be9b820791be4dfea448f6fe7642c
Anrich Potgieter <anrichp@gmail.com>
2021-03-30 18:19:22 +0100

    experiment

--- online store/app.py
+++ online store/app.py
@@ -1,14 +1,14 @@
-from user import Orginisation
+from user import Individual
 from base import Session, engine, Base
 
 Base.metadata.create_all(engine)
 
 session = Session()
 
-Orginisation.createUser(first_name='Quintus', last_name='Potgieter', email_address='quintus@gmail.com',
-                        contact_number='0736229189', company_name='test company', tax_certificate='1234')
+Individual.createUser(first_name='Quintus', last_name='Potgieter', email_address='quintus@gmail.com',
+                        contact_number='0736229189',proof_of_identity='identity', proof_of_banking='banking' )
 
-users = session.query(Orginisation).all()
+users = session.query(Individual).all()
 
 for user in users:
-    print(user.first_name, user.last_name, user.company_name)
+    print(user.first_name, user.last_name, user.type)
