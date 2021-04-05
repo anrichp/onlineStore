f27680f8220c484bbed82451fb27454910222d0d
Anrich Potgieter <anrichp@gmail.com>
2021-04-05 23:41:29 +0100

    losing my mind

--- online store/app.py
+++ online store/app.py
@@ -4,6 +4,3 @@ from users import Customer
 
 Base.metadata.create_all(engine, checkfirst=True)
 
-quintus = Customer('Quintus', 'Potgieter', 'anrichp@gmail.com', '07907451834')
-o1 = Order(quintus)
-
