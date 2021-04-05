b72b92a4b345d9b70157fe47e1553d3548b7a6e0
Anrich Potgieter <anrichp@gmail.com>
2021-04-05 22:47:20 +0100

    new database

--- online store/base.py
+++ online store/base.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///:memory:', echo=True)
+engine = create_engine('postgresql://postgres:Z3ff3r@$zaq@localhost:5432/onlinestore', echo=True)
 
 Base = declarative_base()
 Base.metadata.create_all(engine)
