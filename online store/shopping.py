5a54bbd438b22aebd6cf3ec266e91cbca7c74c76
Anrich Potgieter <anrichp@gmail.com>
2021-04-05 13:52:41 +0100

    create classes to modify the shopping basket

--- online store/shopping.py
+++ online store/shopping.py
@@ -9,3 +9,12 @@ class ShoppingBasket(Base):
     __tablename__ = 'ShoppingBasket'
 
     created_date = Column(DateTime, default=datetime.datetime.utcnow)
+
+    def checkout(self, products):
+        pass
+
+    def update(self, products):
+        pass
+
+    def delete(self, products):
+        pass
