f1e69010a2ae8de585b6fcea98978a58437c6af1
Anrich Potgieter <anrichp@gmail.com>
2021-04-05 21:41:53 +0100

    testing code

--- online store/app.py
+++ online store/app.py
@@ -1,5 +1,4 @@
 from user import User, Customer
-from shopping import Order
 from base import Session, engine, Base
 
 Base.metadata.create_all(engine)
@@ -7,9 +6,4 @@ Base.metadata.create_all(engine)
 session = Session()
 
 quintus = Customer(first_name='Quintus', last_name='Potgieter', email_address='quintus@gmail.com',
-                      contact_number='0736229189')
-
-o1 = Order(customer = quintus)
-
-for user in users:
-    print(user.first_name, user.last_name, user.type)
+                   contact_number='0736229189')
