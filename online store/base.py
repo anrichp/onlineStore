d4af7efe0565384bad0a9f47029540618c525c80
Anrich Potgieter <anrichp@gmail.com>
2021-04-05 22:29:12 +0100

    fix base class

--- online store/base.py
+++ online store/base.py
@@ -3,10 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('sqlite:///:memory:', echo=True)
-Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
 Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
 
 from shopping import *
 from users import *
