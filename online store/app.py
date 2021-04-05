4e2a9181c9a0c53b420a017c80421e41e5984da5
Anrich Potgieter <anrichp@gmail.com>
2021-04-05 21:22:00 +0100

    remove query

--- online store/app.py
+++ online store/app.py
@@ -1,4 +1,4 @@
-from user import Customer, User
+from user import Customer
 from shopping import Order
 from base import Session, engine, Base
 from sqlalchemy.orm import relationship
@@ -9,8 +9,6 @@ session = Session()
 
 quintus = Customer(first_name='Quintus', last_name='Potgieter', email_address='quintus@gmail.com',
                       contact_number='0736229189')
-
-quintus.searchProduct('laptop')
 
 for user in users:
     print(user.first_name, user.last_name, user.type)
