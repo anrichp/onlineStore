015b694ed6a65526b78a7033fd4d18f856214f53
Anrich Potgieter <anrichp@gmail.com>
2021-04-06 00:02:48 +0100

    foreig key fix

--- online store/app.py
+++ online store/app.py
@@ -1,6 +1,13 @@
 from base import Session, engine, Base
-from users import *
-from shopping import *
+from users import Customer
+from shopping import Order
 
+session = Session()
 
+Base.metadata.create_all(engine, checkfirst=True)
 
+quintus = Customer('Anrich', 'Quintus', 'anrichp@gmail.com', '07907451834')
+
+session.add(quintus)
+
+session.commit()
