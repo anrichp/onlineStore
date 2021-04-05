c9a170c7475a0def4b44839d5a10800be48c5f57
Anrich Potgieter <anrichp@gmail.com>
2021-04-05 22:59:28 +0100

    fix imports

--- online store/users.py
+++ online store/users.py
@@ -36,7 +36,7 @@ class Customer(User):
     shipping_address = Column(String(50))
     billing_address = Column(String(50))
     payment_details = Column(String(50))
-    orders = relationship('Order', backref='User')
+    orders = relationship('Order', backref='customer')
 
     def addToShoppingBasket(product):
         basket_item = ShoppingBasket(product=product)
