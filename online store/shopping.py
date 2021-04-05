e4481b7dcca5192322f3172a22e29cd58d0ffc0c
Anrich Potgieter <anrichp@gmail.com>
2021-04-05 21:17:57 +0100

    update foreign key

--- online store/shopping.py
+++ online store/shopping.py
@@ -25,7 +25,7 @@ class Order(Base):
     __tablename__ = 'Order'
 
     order_id = Column(Integer, primary_key=True)
-    customer_id = Column(Integer, ForeignKey('User.user_id'))
+    customer_id = Column(Integer, ForeignKey('Customer.user_id'))
     date_placed = Column(DateTime, default=datetime.datetime.utcnow)
     total = Column(Numeric(12, 2), nullable=False)
     order_status = Column(String(10), nullable=False)
