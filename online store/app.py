a45d4dd7d90989879380c9ac0ec4060d3d1abb5a
Anrich Potgieter <anrichp@gmail.com>
2021-04-05 23:41:05 +0100

    fixing foreign key

--- online store/app.py
+++ online store/app.py
@@ -1,6 +1,8 @@
 from base import Session, engine, Base
-from users import Customer
 from shopping import Order
+from users import Customer
+
+Base.metadata.create_all(engine, checkfirst=True)
 
 quintus = Customer('Quintus', 'Potgieter', 'anrichp@gmail.com', '07907451834')
 o1 = Order(quintus)
--- online store/base.py
+++ online store/base.py
@@ -6,7 +6,6 @@ from sqlalchemy.orm import sessionmaker
 engine = create_engine('postgresql://postgres:Z3ff3r@$zaq@localhost:5432/onlinestore', echo=True)
 
 Base = declarative_base()
-Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
 session = Session()
