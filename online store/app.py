ac8ecdf80832c7c0e687c289dff39e974a68a3ae
Anrich Potgieter <anrichp@gmail.com>
2021-04-06 21:06:34 +0100

    testing code

--- online store/app.py
+++ online store/app.py
@@ -1,7 +1,7 @@
 from base import Session, engine, Base
-from product import Product, ProductCatalogue
+from product import *
 from users import *
-from shopping import Order
+from shopping import *
 
 
 session = Session()
@@ -9,3 +9,10 @@ session = Session()
 Base.metadata.create_all(engine, checkfirst=True)
 
 anrich = Seller('Anrich', 'Potgieter', 'anrichp@gmail.com', '07907451834')
+product1 = Product('Laptop', 'Description', '25')
+
+anrich_catalogue = ProductCatalogue()
+anrich_catalogue.user = anrich
+anrich_catalogue.products = product1
+session.add(anrich_catalogue)
+session.commit()
