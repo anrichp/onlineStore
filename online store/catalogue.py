e588b5c295404d0756cc73f67775223bb4865f31
Anrich Potgieter <anrichp@gmail.com>
2021-04-08 22:48:36 +0100

    update product table with required columns and relationships

--- online store/catalogue.py
+++ online store/catalogue.py
@@ -14,6 +14,22 @@ class Product(Base):
     product_title = Column(String(50), nullable=False)
     product_description = Column(String(120), nullable=False)
     product_price = Column(Numeric(12, 2), nullable=False)
+    category_category_id = Column(Integer, ForeignKey('category.category_id'))
+    location_location_id = Column(Integer, ForeignKey('location.location_id'))
+    quantity_quantity_id = Column(Integer, ForeignKey('quantity.quantity_id'))
+    productstatus_status_id = Column(
+        Integer, ForeignKey('productStatus.status_id'))
+    productcatalogue_catalogue_id = Column(
+        Integer, ForeignKey('productCatalogue.catalogue_id'))
+
+    # Relationships
+    category = relationship('category', foreign_keys=category_category_id)
+    location = relationship('location', foreign_keys=location_location_id)
+    quantity = relationship('quantity', foreign_keys=quantity_quantity_id)
+    productstatus = relationship(
+        'productStatus', foreign_keys=productstatus_status_id)
+    productcatalogue = relationship(
+        'productCatalogue', foreign_keys=productcatalogue_catalogue_id)
 
 
 class ProductCatalogue(Base):
@@ -25,9 +41,9 @@ class ProductCatalogue(Base):
     # Relationships
     user = relationship('User', backref='productCatalogue',
                         foreign_keys=seller_id)
-    products = relationship("Product", secondary=association_table)
+    products = relationship('Product', secondary=association_table)
 
-    @classmethod
+    @ classmethod
     def createProduct(cls, **kw):
         obj = cls(**kw)
         session.add(obj)
