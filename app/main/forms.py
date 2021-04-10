a6db8c6d4402007f1a6fa8fe24b01a5f54a50cd8
Anrich Potgieter <anrichp@gmail.com>
2021-04-10 18:17:36 +0100

    fix labels

--- app/main/forms.py
+++ app/main/forms.py
@@ -23,17 +23,17 @@ class NewProduct(FlaskForm):
         return db.session.query(ProductCatalogue)
 
     product_catalogue = QuerySelectField(
-        get_label='name', query_factory=productStatusQuery())
+        get_label='catalogue_id', query_factory=productStatusQuery())
     product_name = StringField(
         'Name', validators=[Length(0, 80), InputRequired()])
     product_description = StringField('Description', validators=[
                                       Length(0, 250), InputRequired()])
     product_price = IntegerField(
         'Price', validators=[NumberRange(min=0, max=10000), InputRequired()])
-    category = QuerySelectField(get_label='name', query_factory=categoryQuery)
+    category = QuerySelectField(get_label='category_name', query_factory=categoryQuery)
     quantity = IntegerField('Quantity', validators=[
                             NumberRange(min=0, max=100), InputRequired()])
-    locaton = QuerySelectField(get_label='name', query_factory=locationQuery)
+    locaton = QuerySelectField(get_label='product_location', query_factory=locationQuery)
     status = QuerySelectField(
-        get_label='name', query_factory=productStatusQuery())
+        get_label='product_status', query_factory=productStatusQuery())
     submit = SubmitField('Submit')
