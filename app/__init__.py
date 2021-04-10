2508f9dc657166197b548d8c7d5095e52d0e9a8a
Anrich Potgieter <anrichp@gmail.com>
2021-04-10 15:41:20 +0100

    fix blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,7 @@
-from flask import Flask, current_app, redirect, session, url_for
+from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from config import config
-import httplib2
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -15,5 +14,8 @@ def create_app(config_name):
 
     bootstrap.init_app(app)
     db.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
     return app
