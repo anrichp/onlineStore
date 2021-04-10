82ec928d643c20557b2f29373a24f5eda23b5ea7
Anrich Potgieter <anrichp@gmail.com>
2021-04-10 17:14:32 +0100

    completing new product form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, BooleanField, SelectField,\
-    SubmitField
-from wtforms.validators import DataRequired, Length, Email, Regexp
+    SubmitField, IntegerField
+from wtforms.validators import DataRequired, Length, NumberRange, Email, Regexp
 from wtforms import ValidationError
 from flask_pagedown.fields import PageDownField
 from ..models import Product
@@ -19,6 +19,8 @@ class NewProduct(FlaskForm):
 
     product_name  = StringField('Name', validators=[Length(0,80), InputRequired()])
     product_description = StringField('Description', validators=[Length(0, 250), InputRequired()])
+    product_price = IntegerField('Price', validators=[NumberRange])
     category = QuerySelectField(get_label='name', query_factory=categoryQuery)
     locaton = QuerySelectField(get_label='name', query_factory=locationQuery)
     status = QuerySelectField(get_label='name', query_factory=productStatusQuery())
+    submit - SubmitField('Submit')
