df060089c46bd76b1b1698b0e3fd5e29d7fc0777
Anrich Potgieter <anrichp@gmail.com>
2021-04-10 13:56:05 +0100

    view import statments

---
+++ online store/app/main/views.py
@@ -0,0 +1,6 @@
+from flask import render_template, redirect, url_for, jsonify, session
+from flask import request
+from . import main
+from .. import db
+from ..models import User, Product, ProductCatalogue, ShoppingBasket, Order
+
