a3c5bef5fb656c0259421507b6ec7147eb6f8464
Anrich Potgieter <anrichp@gmail.com>
2021-04-10 15:14:42 +0100

    fix render template

--- online store/app/main/views.py
+++ online store/app/main/views.py
@@ -6,4 +6,4 @@ from ..models import User, Product, ProductCatalogue, ShoppingBasket, Order
 
 @main.route('/')
     def index():
-        return render_template(index.html)
+        return render_template('index.html')
