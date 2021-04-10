bd7d2b31bea7009201f1f415d8bf41ed3ea0bb03
Anrich Potgieter <anrichp@gmail.com>
2021-04-10 15:09:42 +0100

    append db to all models

--- online store/app/models.py
+++ online store/app/models.py
@@ -6,12 +6,12 @@ from . import db
 class User(db.Model):
     __tablename__ = 'user'
 
-    user_id = Column(Integer, primary_key=True)
-    first_name = Column(String(50), nullable=False)
-    last_name = Column(String(50), nullable=False)
-    email_address = Column(String(120), unique=True, nullable=False)
-    contact_number = Column(String(20), unique=True)
-    type = Column(String(20))
+    user_id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email_address = db.Column(db.String(120), unique=True, nullable=False)
+    contact_number = db.Column(db.String(20), unique=True)
+    type = db.Column(db.String(20))
 
     __mapper_args__ = {
         'polymorphic_on': type,
@@ -33,10 +33,10 @@ class User(db.Model):
 
 
 class Customer(User):
-    shipping_address = Column(String(50))
-    billing_address = Column(String(50))
-    payment_details = Column(String(50))
-    orders = relationship('Order', backref='user')
+    shipping_address = db.Column(db.String(50))
+    billing_address = db.Column(db.String(50))
+    payment_details = db.Column(db.String(50))
+    orders = db.relationship('Order', backref='user')
 
     def addToShoppingBasket(product):
         basket_item = ShoppingBasket(product=product)
@@ -52,7 +52,7 @@ class Customer(User):
 
 
 class WarehouseStaff(User):
-    job_title = Column(String(50))
+    job_title = db.Column(db.String(50))
 
     __mapper_args__ = {
         'polymorphic_identity': 'warehouseStaff'
@@ -60,7 +60,7 @@ class WarehouseStaff(User):
 
 
 class Seller(User):
-    store_name = Column(String(50))
+    store_name = db.Column(db.String(50))
 
     @classmethod
     def createCatalogue(cls, **kw):
@@ -89,8 +89,8 @@ class ThirdPartySeller(Seller):
 
 
 class Individual(ThirdPartySeller):
-    proof_of_identity = Column(String(50))
-    proof_of_banking = Column(String(50))
+    proof_of_identity = db.Column(db.String(50))
+    proof_of_banking = db.Column(db.String(50))
 
     def __init__(self, first_name, last_name, email_address, contact_number, proof_of_identity, proof_of_banking):
         self.first_name = first_name
@@ -106,8 +106,8 @@ class Individual(ThirdPartySeller):
 
 
 class Orginisation(ThirdPartySeller):
-    company_name = Column(String(50))
-    tax_certificate = Column(String(50))
+    company_name = db.Column(db.String(50))
+    tax_certificate = db.Column(db.String(50))
 
     def __init__(self, first_name, last_name, email_address, contact_number, company_name, tax_certificate):
         self.first_name = first_name
@@ -122,72 +122,72 @@ class Orginisation(ThirdPartySeller):
     }
 
 #  Product Model
-association_table = Table('product_catalogue', Base.metadata, Column('productCatalogue_id', Integer, ForeignKey(
-    'productCatalogue.catalogue_id')), Column('product_id', Integer, ForeignKey('product.product_id')))
+association_table = Table('product_catalogue', db.metadata, db.Column('productCatalogue_id', db.Integer, db.ForeignKey(
+    'productCatalogue.catalogue_id')), db.Column('product_id', db.Integer, db.ForeignKey('product.product_id')))
 
 
 class Product(db.Model):
     __tablename__ = 'product'
 
-    product_id = Column(Integer, primary_key=True)
-    product_title = Column(String(50), nullable=False)
-    product_description = Column(String(120), nullable=False)
-    product_price = Column(Numeric(12, 2), nullable=False)
-    category_category_id = Column(Integer, ForeignKey('category.category_id'))
-    location_location_id = Column(Integer, ForeignKey('location.location_id'))
-    quantity_quantity_id = Column(Integer, ForeignKey('quantity.quantity_id'))
-    productstatus_status_id = Column(
-        Integer, ForeignKey('productStatus.status_id'))
-    productcatalogue_catalogue_id = Column(
-        Integer, ForeignKey('productCatalogue.catalogue_id'))
+    product_id = db.Column(db.Integer, primary_key=True)
+    product_title = db.Column(db.String(50), nullable=False)
+    product_description = db.Column(db.String(120), nullable=False)
+    product_price = db.Column(db.Numeric(12, 2), nullable=False)
+    category_category_id = db.Column(db.Integer, db.ForeignKey('category.category_id'))
+    location_location_id = db.Column(db.Integer, db.ForeignKey('location.location_id'))
+    quantity_quantity_id = db.Column(db.Integer, db.ForeignKey('quantity.quantity_id'))
+    productstatus_status_id = db.Column(
+        db.Integer, db.ForeignKey('productStatus.status_id'))
+    productcatalogue_catalogue_id = db.Column(
+        db.Integer, db.ForeignKey('productCatalogue.catalogue_id'))
 
     # Relationships
-    category = relationship('category', foreign_keys=category_category_id)
-    location = relationship('location', foreign_keys=location_location_id)
-    quantity = relationship('quantity', foreign_keys=quantity_quantity_id)
-    productstatus = relationship(
+    category = db.relationship('category', foreign_keys=category_category_id)
+    location = db.relationship('location', foreign_keys=location_location_id)
+    quantity = db.relationship('quantity', foreign_keys=quantity_quantity_id)
+    productstatus = db.relationship(
         'productStatus', foreign_keys=productstatus_status_id)
-    productcatalogue = relationship(
+    productcatalogue = db.relationship(
         'productCatalogue', foreign_keys=productcatalogue_catalogue_id)
 
 class Category(db.Model):
     __tablename__ = 'category'
 
-    category_id = Column(Integer, primary_key=True)
-    category_name = Column(String(50), nullable=False)
+    category_id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(50), nullable=False)
 
 
 class Location(db.Model):
     __tablename__ = 'location'
 
-    location_id = Column(Integer, primary_key=True)
-    product_location = Column(String(50), nullable=False)
+    location_id = db.Column(db.Integer, primary_key=True)
+    product_location = db.Column(db.String(50), nullable=False)
 
 
 class Quantity(db.Model):
     __tablename__ = 'quantity'
 
-    quantity_id = Column(Integer, primary_key=True)
-    quantity = Column(Numeric(20), nullable=False)
+    quantity_id = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Numeric(20), nullable=False)
 
 
 class ProductStatus(db.Model):
     __tablename__ = 'productStatus'
 
-    status_id = Column(Integer, primary_key=True)
-    product_status = Column(String(50), nullable=False)
+    status_id = db.Column(db.Integer, primary_key=True)
+    product_status = db.Column(db.String(50), nullable=False)
 
 # Product Catalogue Model
 class ProductCatalogue(db.Model):
     __tablename__ = 'productCatalogue'
 
-    catalogue_id = Column(Integer, primary_key=True)
-    seller_id = Column(Integer, ForeignKey('user.user_id'))
+    catalogue_id = db.Column(db.Integer, primary_key=True)
+    seller_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
 
     # Relationships
-    user = relationship('User', backref='productCatalogue',
+    user = db.relationship('User', backref='productCatalogue',
                         foreign_keys=seller_id)
-    products = relationship('Product', secondary=association_table)
+    products = db.relationship('Product', secondary=association_table)
 
     @ classmethod
     def createProduct(cls, **kw):
@@ -200,15 +200,15 @@ class ProductCatalogue(db.Model):
 class ShoppingBasket(db.Model):
     __tablename__ = 'shoppingBasket'
 
-    basket_id = Column(Integer, primary_key=True)
-    customer_user_id = Column(Integer, ForeignKey('user.user_id'))
-    created_date = Column(DateTime, default=datetime.datetime.utcnow)
-    quantity = Column(Numeric(10))
-    total_cost = Column(Numeric(12, 2), nullable=False)
+    basket_id = db.Column(db.Integer, primary_key=True)
+    customer_user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    created_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    quantity = db.Column(db.Numeric(10))
+    total_cost = db.Column(db.Numeric(12, 2), nullable=False)
 
     # Relationships
-    user = relationship('User', backref='shoppingBasket', foreign_keys=customer_user_id)
-    products = relationship('Product', backref='shoppingBasket')
+    user = db.relationship('User', backref='shoppingBasket', foreign_keys=customer_user_id)
+    products = db.relationship('Product', backref='shoppingBasket')
 
     def checkout(self, products):
         pass
@@ -224,8 +224,8 @@ class ShoppingBasket(db.Model):
 class Order(db.Model):
     __tablename__ = 'order'
 
-    order_id = Column(Integer, primary_key=True)
-    date_placed = Column(DateTime, default=datetime.datetime.utcnow)
+    order_id = db.Column(db.Integer, primary_key=True)
+    date_placed = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     # total = Column(Numeric(12, 2), nullable=False)
-    order_status = Column(String(10), nullable=False)
-    customer_id = Column(Integer, ForeignKey('user.user_id'))
+    order_status = db.Column(db.String(10), nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
