fbb1be8ae83d33e4f7ffc9007b99fef30dac5160
Anrich Potgieter <anrichp@gmail.com>
2021-04-10 17:38:06 +0100

    create new product decrator

--- app/main/views.py
+++ app/main/views.py
@@ -5,10 +5,20 @@ from .forms import NewProduct
 from .. import db
 from ..models import User, Product, ProductCatalogue, ShoppingBasket, Order
 
+
 @main.route('/')
 def index():
     return render_template('index.html')
 
+
 @main.route('/newProduct', methods=['GET', 'POST'])
 def newProduct():
-    pass
+    form = NewProduct()
+
+    product = Product(product_title=form.product_name.data, product_description=form.product_description.data, product_price=form.product_price.data, category_category_id=form.category.data,
+                      location_location_id=form.locaton.data, quantity_quantity_id=form.quantity.data, productstatus_status_id=form.status.data, productcatalogue_catalogue_id=form.product_catalogue)
+
+    db.sesson.add(product)
+    db.commit()
+
+    return render_template('newProduct.html', form=form)
