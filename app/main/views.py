22b1c4b89046d5a39bfa175fdf978eb8705ed4f7
Anrich Potgieter <anrichp@gmail.com>
2021-04-10 18:33:45 +0100

    missing context

--- app/main/views.py
+++ app/main/views.py
@@ -15,7 +15,7 @@ def index():
 def newProduct():
     form = NewProduct()
 
-    product = Product(product_title=form.product_name.data, product_description=form.product_description.data, product_price=form.product_price.data, category_category_id=form.category.data,
+    product = Product(product_title=form.product_name.data, product_description=form.product_description.data, Category=Category, product_price=form.product_price.data, category_category_id=form.category.data,
                       location_location_id=form.locaton.data, quantity_quantity_id=form.quantity.data, productstatus_status_id=form.status.data, productcatalogue_catalogue_id=form.product_catalogue)
 
     db.session.add(product)
