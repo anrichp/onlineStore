8b69aeaeb88c6f2f180bd77208d42ffb80c7157f
Anrich Potgieter <anrichp@gmail.com>
2021-04-11 23:48:07 +0100

    missing db

--- app/models.py
+++ app/models.py
@@ -189,9 +189,9 @@ class OrderLine(db.Model):
     __tablename__ = 'orderLines'
 
     orderlines_id =  db.Column(db.Integer, primary_key=True)
-    order_id = db.Column(Integer, db.ForeignKey('order.order_id'))
+    order_id = db.Column(db.Integer, db.ForeignKey('order.order_id'))
     item_id = db.Column(db.Integer, db.ForeignKey('product.product_id'))
-    quantity = Column(SmallInteger())
+    quantity = db.Column(SmallInteger())
     product = db.relationship("Product")
 
 
