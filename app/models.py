7b58f759a82665b47cd943a6e955de8b9737ebf3
Anrich Potgieter <anrichp@gmail.com>
2021-04-11 23:52:01 +0100

    update id to product_id

--- app/models.py
+++ app/models.py
@@ -190,7 +190,7 @@ class OrderLine(db.Model):
 
     orderlines_id =  db.Column(db.Integer, primary_key=True)
     order_id = db.Column(db.Integer, db.ForeignKey('order.order_id'))
-    item_id = db.Column(db.Integer, db.ForeignKey('product.product_id'))
+    product_id = db.Column(db.Integer, db.ForeignKey('product.product_id'))
     quantity = db.Column(db.Numeric(10,0))
     product = db.relationship("Product")
 
