6efcfd50c97c41e3311c6b64772434f0cf259ff0
Anrich Potgieter <anrichp@gmail.com>
2021-04-11 19:30:04 +0100

    update view to reflect the quantity model change

--- app/main/views.py
+++ app/main/views.py
@@ -33,10 +33,8 @@ def newProduct():
 
     if request.method == 'POST' and form.validate_on_submit():
 
-        product = Product(product_title=form.product_name.data, product_description=form.product_description.data, product_price=form.product_price.data, category=form.category.data,
+        product = Product(product_title=form.product_name.data, product_description=form.product_description.data, product_price=form.product_price.data, product_quantity=form.quantity.data, category=form.category.data,
                           location=form.location.data, productstatus=form.status.data, productcatalogue=form.product_catalogue.data)
-
-        quantity = Quantity(product=product, quantity=form.quantity.data)
 
         db.session.add(quantity)
         db.session.commit()
