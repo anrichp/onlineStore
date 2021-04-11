23eebf6d3658f8602f9100b4710302f39032d8a1
Anrich Potgieter <anrichp@gmail.com>
2021-04-11 17:33:58 +0100

    update config

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+        'mysql://root:Z3ff3r@$zaq@localhost/onlinestore'
 
 
 class TestingConfig(Config):
