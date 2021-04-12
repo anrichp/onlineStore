263a4d633ac9939bdf748044efd4e4a856399148
Anrich Potgieter <anrichp@gmail.com>
2021-04-12 22:29:33 +0100

    append product

--- populateDB.py
+++ populateDB.py
@@ -78,7 +78,7 @@ db.session.add(outStock)
 db.session.add(Quintus)
 db.session.add_all([in_progress, processing, pending_payment,
                    awaiting_picking, ready_for_delivery, shipped])
-db.session.add_all([p1, p2])
+db.session.add_all([p1, p2, p3])
 
 db.session.commit()
 
