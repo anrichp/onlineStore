f41dec0c758abf9b811c6f36918f8baf1b858221
Anrich Potgieter <anrichp@gmail.com>
2021-04-12 18:40:35 +0100

    no need for catalogue item

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
-    CATALOG_ADMIN = os.environ.get('CATALOG_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PROJECT_ID = 'onlinestore'
 
